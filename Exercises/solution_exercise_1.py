2693db661bf526b0f2b8b12546753beb819aa86c
Marta <nevermind@gmail.com>
2018-10-14 19:52:27 +0200

    Update exercise files

--- Exercises/solution_exercise_1.py
+++ Exercises/solution_exercise_1.py
@@ -1,8 +1,3 @@
-# Exercise 1
-# If you run this file you'll se a NameError: name 'function_one' is not defined
-# Modify below file in a way which allows you to use functions from first_module and second_module
-# Do not use other import metod, try to modify the file body
-
 import first_module
 
 
